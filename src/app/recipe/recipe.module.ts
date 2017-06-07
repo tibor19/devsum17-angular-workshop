900abdaaeb68491aa82c18e3c8c63d2cf88ee577
Tibi Covaci <tibi@covaci.se>
2017-06-07 11:00:57 +0200

    Add the recipe module Add the components

---
+++ src/app/recipe/recipe.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from '../home/home.component';
+import { WeeklyMenuComponent } from '../weekly-menu/weekly-menu.component';
+import { RecipeComponent } from './recipe.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [HomeComponent, WeeklyMenuComponent, RecipeComponent]
+})
+export class RecipeModule { }
